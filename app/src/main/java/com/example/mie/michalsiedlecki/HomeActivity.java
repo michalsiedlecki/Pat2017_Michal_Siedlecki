139705a9809348d6181146273a9ddef4250423a4
Michał Siedlecki <michal.siedlecki@intive.com>
2017-01-27 11:48:34 +0100

    Spełnienie warunków podstawowych + progressbar na pojedynczych zdjęciach

--- app/src/main/java/com/example/mie/michalsiedlecki/HomeActivity.java
+++ app/src/main/java/com/example/mie/michalsiedlecki/HomeActivity.java
@@ -1,17 +1,65 @@
 package com.example.mie.michalsiedlecki;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.mie.michalsiedlecki.Models.HomeModel;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ListView homeListView;
+    //IP for emulator
+    //public final static String BASE_SERVER_URL = "http://10.0.2.2:8080/page_0.json";
+    //IP for phone
+    public final static String BASE_SERVER_URL = "http://192.168.15.36:8080/page_0.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+        .cacheInMemory(true)
+                .cacheOnDisk(true)
+        .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+        .defaultDisplayImageOptions(defaultOptions)
+        .build();
+        ImageLoader.getInstance().init(config); // Do it on Application start
+
+        homeListView = (ListView)findViewById(R.id.homeListView);
+        new JSONTask().execute(BASE_SERVER_URL);
     }
 
     public void onLogoutButtonClick(View view) {
@@ -30,5 +78,130 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public class  JSONTask extends AsyncTask<String, String, List<HomeModel>>{
+
+        @Override
+        protected List<HomeModel> doInBackground(String... params){
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line ="";
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+
+                String finalJSON = buffer.toString();
+
+                JSONObject parentObject = new JSONObject(finalJSON);
+                JSONArray parentArray = parentObject.getJSONArray("array");
+
+                List<HomeModel> homeModelList = new ArrayList<>();
+
+                for (int i=0; i<parentArray.length(); i++){
+                    JSONObject finalObject = parentArray.getJSONObject(i);
+                    HomeModel homeModel = new HomeModel();
+                    homeModel.setUrl(finalObject.getString("url"));
+                    homeModel.setTitle(finalObject.getString("title"));
+                    homeModel.setDesc(finalObject.getString("desc"));
+                    homeModelList.add(homeModel);
+                }
+
+                return homeModelList;
+
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally{
+                if (connection != null){
+                    connection.disconnect();
+                }
+                try{
+                    if (reader != null){
+                        reader.close();
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(final List<HomeModel> result) {
+            super.onPostExecute(result);
+            if (result != null){
+                ItemAdapter adapter = new ItemAdapter(getApplicationContext(), R.layout.list_item, result);
+                homeListView.setAdapter(adapter);
+            }
+
+        }
+    }
+
+    public class ItemAdapter extends ArrayAdapter{
+
+        private List<HomeModel> homeModelList;
+        private int resource;
+        private LayoutInflater infalter;
+        public ItemAdapter(Context context, int resource, List<HomeModel> objects) {
+            super(context, resource, objects);
+            homeModelList = objects;
+            this.resource = resource;
+            infalter = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if(convertView == null){
+                convertView = infalter.inflate(resource, null);
+            }
+
+            ImageView imageView;
+            TextView titleTextView;
+            TextView descTextView;
+            final ProgressBar progressBar;
+
+            imageView = (ImageView)convertView.findViewById(R.id.itemImageView);
+            titleTextView = (TextView)convertView.findViewById(R.id.titleTextView);
+            descTextView = (TextView)convertView.findViewById(R.id.descTextView);
+            progressBar = (ProgressBar)convertView.findViewById(R.id.progressBar);
+
+            ImageLoader.getInstance().displayImage(homeModelList.get(position).getUrl(), imageView, new ImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String imageUri, View view) {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onLoadingCancelled(String imageUri, View view) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            });
+            titleTextView.setText(homeModelList.get(position).getTitle());
+            descTextView.setText(homeModelList.get(position).getDesc());
+            return convertView;
+        }
     }
 }
---
+++ app/src/main/java/com/example/mie/michalsiedlecki/Models/HomeModel.java
@@ -0,0 +1,39 @@
+package com.example.mie.michalsiedlecki.Models;
+
+/**
+ * Created by MIE on 26-Jan-17.
+ */
+
+public class HomeModel {
+
+    private String title;
+    private String desc;
+    private String url;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+
+}
